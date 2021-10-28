620ed2c496292506c8d82e7e2f9a727d68c7aca9
Brian <swixee@gmail.com>
2021-10-28 15:21:03 +0000

    user-setup now takes user as input

--- user-setup.sh
+++ user-setup.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-adduser brian sudo
-cat bashrc >> /home/brian/.bashrc
-cat vimrc >> /home/brian/.vimrc
-chown -hR brian:brian /home/brian/
+
+show_usage()
+{
+  echo "Usage:"
+  echo "user-setup.sh <username>"
+  echo "  Creates user, adds to sudo group, and sets up bashrc and vimrc"
+  echo "  Must be run as root"
+}
+
+USER=$1
+if [ -z "$1" ]; then
+  echo "No user specified, exiting\n"
+  show_usage
+  exit 1
+fi
+
+adduser $USER
+adduser $USER sudo
+HOME_DIR=/home/$USER
+echo "Appending bashrc >> $HOME_DIR/.bashrc"
+cat bashrc >> $HOME_DIR/.bashrc
+echo "Appending vimrc >> $HOME_DIR/.vimrc"
+cat vimrc >> $HOME_DIR/.vimrc
+echo "Giving ownership of $HOME_DIR/ to $USER"
+chown -hR brian:brian $HOME_DIR/
+echo "Done."
